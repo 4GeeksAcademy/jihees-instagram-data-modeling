f76274fe6ee61c80587ec92459094ac1a6481548
Jahn1295 <jiheeahn1222@gmail.com>
2024-09-13 18:02:49 +0000

    finish

--- src/models.py
+++ src/models.py
@@ -1,29 +1,64 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    username = Column(String(250), nullable=False, unique=True)
+    password = Column(String(256), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class FollowReq(Base):
+    __tablename__ = 'follow_req'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    follower_id = Column(Integer, ForeignKey('user.id'))
+    followed_id = Column(Integer, ForeignKey('user.id'))
+    accepted = Column(Boolean, nullable=False)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    text = Column(String(250), nullable=False)
+    media = Column(String(512), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comments = relationship('Comment', backref='post') #one-many relationships with comments
+    likes = relationship('PostLike', backref='post')
+
+class PostLike(Base):
+    __tablename__= 'post_like'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    likes = relationship('CommentLike', backref='comment')
+    dislikes = relationship('CommentDislike', backref='comment')
+
+class CommentLike(Base):
+    __tablename__ = 'comment_like'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comment_id = Column(Integer, ForeignKey('comment.id'))
+
+class CommentDislike(Base):
+    __tablename__ = 'comment_dislike'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comment_id = Column(Integer, ForeignKey('comment.id'))
+
 
     def to_dict(self):
         return {}
